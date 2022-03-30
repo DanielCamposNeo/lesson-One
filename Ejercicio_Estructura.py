4fa0f5de4ff0c4343e2e7127670984a446012da6
DanielCamposNeo <p-dcampos@neoris.com>
2022-03-30 12:08:27 +0200

    update

---
+++ Ejercicio_Estructura.py
@@ -0,0 +1,9 @@
+tupla = ("daniel","3/30/2022",23,"Madrid")
+type(tupla)
+lista = list(tupla)
+type (lista)
+diccionario = {"1":lista[0],
+             "2":lista[1],
+             "3":lista[2],
+             "4":lista[3]}
+print (diccionario)
