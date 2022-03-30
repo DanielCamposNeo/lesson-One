fd29dd8c2231b231cb3bb383584da00daae5d7bd
DanielCamposNeo <p-dcampos@neoris.com>
2022-03-30 16:09:33 +0200

    update

---
+++ ejercicio_archivos.py
@@ -0,0 +1,28 @@
+import pandas as pb
+import numpy as np
+import csv
+
+datos = pb.read_csv("athlete_events.csv", index_col=0)
+medalla = datos[datos["Medal"].notna()]
+
+print("Competidor mas veterano en recibir una medalla de oro")
+print(medalla[medalla["Medal"]=="Gold"].max())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("Competidor mas veterano en recibir una medalla de plata")
+print(medalla[medalla["Medal"]=="Silver"].max())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("Competidor mas veterano en recibir una medalla de bronce")
+print(medalla[medalla["Medal"]=="Bronze"].max())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("Competidor mas Joven en recibir una medalla de Oro")
+print(medalla[medalla["Medal"]=="Gold"].min())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+"""print(datos["Games"].unique().tolist())
+print(datos["Sex"].count())
+print(datos["Sex"].value_counts())
+print(datos[datos["Age"]==10])
+print (datos[datos["Sport"] == "Football"])"""
