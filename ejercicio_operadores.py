e46cdc73e8947c390d5e9161a1a68549529014c0
DanielCamposNeo <p-dcampos@neoris.com>
2022-03-30 12:07:30 +0200

    update

---
+++ ejercicio_operadores.py
@@ -0,0 +1,10 @@
+#1.-El doble de mi edad tiene 24 años, ¿cuántos años tengo?
+edad = 24 / 2
+print("tu edad es de : " , edad)
+#2.- A un tercio de la edad de mi hermana la disminuyo en 15 años, obtengo 6 años. ¿Qué edad tiene?
+operacion = (15 + 6) * 3
+print (operacion)
+#3.- Determina quién es más grande
+tu = 12
+hermana = 63
+print ("Tu hermana es mayor que tu: " ,hermana > tu)
