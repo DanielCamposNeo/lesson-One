85eccd267d8d08aaddc6b3360be9c88ec1ef8668
DanielCamposNeo <p-dcampos@neoris.com>
2022-03-30 12:28:10 +0200

    update

---
+++ ejercicio_mayorEdad.py
@@ -0,0 +1,7 @@
+edad = int(input("¿Cual es tu edad?"))
+if edad >= 18:
+    print("Eres mayor de edad")
+elif edad < 0:
+    print("No se puede tener edad negativa")
+else :
+    print("Eres menor de edad")
