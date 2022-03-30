5e46180a41a8dd34790c85d205cf08f3277ea28b
DanielCamposNeo <p-dcampos@neoris.com>
2022-03-30 12:42:34 +0200

    update

---
+++ ejercicio_mayorMenor.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Introducza un numero"))
+numero2 = int(input("Introducza otro numero"))
+
+if numero1 > numero2:
+    print("El numero menor es : ", numero2 )
+elif numero1 < numero2:
+    print("El numero menor es: ",numero1)
+elif numero1 == numero2:
+    print("Los dos numeros son iguales")
